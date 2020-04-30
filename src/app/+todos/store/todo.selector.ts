95ee58ba4a062a6b126be178bc124bfeee726334
harbirchahal <harbirchahal@hotmail.com>
2020-04-30 10:44:21 +0530

    update app

--- src/app/+todos/store/todo.selector.ts
+++ src/app/+todos/store/todo.selector.ts
@@ -1,29 +1,12 @@
-import { createSelector, createFeatureSelector } from '@ngrx/store';
+import { createFeatureSelector } from '@ngrx/store';
 import { todoAdapter, TodoState, FEATURE_KEY } from './todo.state';
 
-const {
-  selectIds,
-  selectEntities,
-  selectAll,
-  selectTotal,
-} = todoAdapter.getSelectors();
+const selectTodoState =
+  createFeatureSelector<TodoState>(FEATURE_KEY);
 
-const selectTodoState = createFeatureSelector<TodoState>(FEATURE_KEY);
-
-export const selectTodoIds = createSelector(
-  selectTodoState,
-  selectIds
-);
-export const selectTodoEntities = createSelector(
-  selectTodoState,
-  selectEntities
-);
-export const selectAllTodos = createSelector(
-  selectTodoState,
-  selectAll
-);
-
-export const selectTodoTotal = createSelector(
-  selectTodoState,
-  selectTotal
-);
+export const {
+  selectIds: selectTodoIds,
+  selectEntities: selectTodoEntities,
+  selectAll: selectAllTodos,
+  selectTotal: selectTodoTotal,
+} = todoAdapter.getSelectors(selectTodoState);
